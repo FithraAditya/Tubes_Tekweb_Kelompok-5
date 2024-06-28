c6773952297dba0e7715d1c1bf8977b1a68eba1b
Fithra Aditya <137029453+FithraAditya@users.noreply.github.com>
2024-06-28 13:16:56 +0700

    Add files via upload

---
+++ views/pages/user/beranda.php
@@ -0,0 +1,207 @@
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                <h1>O'Teras Cafe</h1>
+                <p style="text-align: justify; color: white;">
+                    Sip Your Serenity: Where Every Cup of Coffee Feels Like Home.
+                </p>
+            </div>
+        </div>
+    </div>
+</section><!-- End Hero -->
+
+<section id="hero2" class="d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 order-2 order-lg-1">
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 d-flex flex-column justify-content-center">
+                <h1>Our Cafe</h1>
+                <p style="text-align: justify; color: black; font-size: 22px;">
+                    Kafe kami, yang berlokasi strategis di dekat kampus, menyambut semua kalangan, terutama mahasiswa, dengan suasana hangat dan nyaman. Kami menyediakan berbagai pilihan menu yang lezat dan terjangkau, mulai dari kopi spesial, camilan, hingga hidangan utama. Dengan fasilitas Wi-Fi gratis dan area belajar yang nyaman, kafe ini menjadi tempat ideal untuk belajar, bersantai, atau berkumpul bersama teman. Kami berkomitmen untuk memberikan pengalaman kuliner terbaik dan menjadi tempat favorit bagi mahasiswa dan masyarakat sekitar.
+                </p>
+            </div>
+        </div>
+    </div>
+</section><!-- End Hero -->
+
+<main id="main">
+    <section id="recomendation" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h1 style="text-align: center; font-weight:bold">Menu Unggulan</h1>
+                <h4 style="text-align: center; color: black; font-size: 26px;">Ragam makanan lezat dari masakan lokal dan internasional, serta minuman segar seperti kopi spesial dan teh aromatik di cafe kami. Juga tersedia paket makanan lengkap dengan pilihan extra untuk pengalaman santap yang memuaskan. </h4>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/Latte.png" alt=""></div>
+                        <h4 class="title"><a href="">Latte</a></h4>
+                        <p class="description">Espresso dengan susu steamed dan sedikit busa susu di atasnya, memberikan rasa kopi yang lembut dan creamy</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/Cappuccino.png" alt=""></div>
+                        <h4 class="title"><a href="">Cappuccino</a></h4>
+                        <p class="description">Perpaduan sempurna antara espresso, susu, dan busa susu yang lembut, memberikan pengalaman kopi yang creamy</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/Matcha.png" alt=""></div>
+                        <h4 class="title"><a href="">Matcha</a></h4>
+                        <p class="description">Teh hijau bubuk dari Jepang yang kaya antioksidan dan rasa pahit ringan.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/IceCream.png" alt=""></div>
+                        <h4 class="title"><a href="">Ice Cream</a></h4>
+                        <p class="description">Makanan penutup dingin yang lembut, tersedia dalam berbagai varian rasa seperti vanilla klasik, cokelat indulgent.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <section id="allmenu" class="contact section-bg1">
+        <div class="container">
+            <div class="section-title">
+                <h1 style="text-align: center; font-weight:bold">Daftar Menu</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 order-1 order-lg-2 d-flex flex-column justify-content-center">
+                    <table class="menu-table">
+                        <tr>
+                            <td class="item">Latte</td>
+                            <td class="price">18K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Cappucino</td>
+                            <td class="price">20K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Kopi Tubruk</td>
+                            <td class="price">15K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Americano</td>
+                            <td class="price">15K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Apple Mint Tea</td>
+                            <td class="price">15K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Lemon Tea</td>
+                            <td class="price">12K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Bobba Milk Tea</td>
+                            <td class="price">10K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Tiramisu Frap</td>
+                            <td class="price">20K</td>
+                        </tr>
+                    </table>
+                </div><br>
+                <div class="col-lg-6 order-1 order-lg-2 d-flex flex-column justify-content-center">
+                    <table class="menu-table">
+                        <tr>
+                            <td class="item">Matcha Tea</td>
+                            <td class="price">15K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Ice Cream</td>
+                            <td class="price">10K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Croissant</td>
+                            <td class="price">18K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Klasik Waffle</td>
+                            <td class="price">18K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Nasi Goreng</td>
+                            <td class="price">15K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Bakmie</td>
+                            <td class="price">12K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Pisang Goreng</td>
+                            <td class="price">10K</td>
+                        </tr>
+                        <tr>
+                            <td class="item">Aneka Dimsum</td>
+                            <td class="price">15K</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h1 style="text-align: center; font-weight:bold">About Us</h1>
+                <h4 style="text-align: justify; color: black; font-size: 22px;">Tim kami, terdiri dari empat mahasiswa guna mengerjakan tugas mata kuliah Teknologi WEB, tim sedang mengembangkan website sederhana untuk kafe sebagai tugas akhir. Menggabungkan keahlian desain grafis, dan pemrograman, kami fokus pada tampilan ramah pengguna, informasi menu lengkap, dan fitur reservasi mudah. Kami berkomitmen menghadirkan inovasi digital untuk mendukung operasional kafe dan meningkatkan kepuasan pelanggan.</h4>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/us/1.png" alt=""></div><br>
+                        <h4 class="title"><a href="">Fithra Aditya S.</a></h4>
+                        <p class="description">3411211083</p>
+                        <p class="description">Cimahi</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/us/2.png" alt=""></div><br>
+                        <h4 class="title"><a href="">Ichsan Zaki H.</a></h4>
+                        <p class="description">3411211091</p>
+                        <p class="description">Cimahi</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/us/3.png" alt=""></div><br>
+                        <h4 class="title"><a href="">Muhamad Hasbia</a></h4>
+                        <p class="description">3411211092</p>
+                        <p class="description">Cimahi</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><img src="<?= $base_url; ?>asset_user/img/4x/us/4.png" alt=""></div><br>
+                        <h4 class="title"><a href="">Angga Moch. Kamal</a></h4>
+                        <p class="description">3411211099</p>
+                        <p class="description">Cimahi</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+</main>
